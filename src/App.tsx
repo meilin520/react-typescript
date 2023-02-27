11f6cc67d21ee253ecd35feed563926993eb6417
MrMerin <2938889184@qq.com>
2023-02-27 23:31:51 +0800

    部分优化调整

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import { useEffect, useState } from 'react'
 import type { FC } from 'react'
 import { Outlet } from 'react-router-dom'
-import { ConfigProvider } from 'antd'
 import './App.scss'
 
 const App: FC = () => {
-  const [count, setCount] = useState(0)
-  useEffect(()=>{
-    console.log(import.meta.env)
-  },[])
   return (
     <Outlet />
   )
