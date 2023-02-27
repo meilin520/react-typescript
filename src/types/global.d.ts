1e51b87e67f338850dae6fd3d152816a89942290
MrMerin <2938889184@qq.com>
2023-02-27 23:23:57 +0800

    添加全局声明导入模块@识别

---
+++ src/types/global.d.ts
@@ -0,0 +1,12 @@
+declare module '@/config';
+declare module '@/utils/http';
+declare module '@/router';
+declare module '@/theme';
+declare module '@/redux';
+declare module '@/types' {
+    import { StoreState } from ".";
+    export { StoreState }
+};
+declare module '@/constants';
+declare module '@/components/DataTable';
+declare module '@/utils/tools';
