b2d88ca2c7aaf96533a0753f9358808156011330
MrMerin <2938889184@qq.com>
2023-03-07 21:29:27 +0800

    路由页面部分导入别名

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -3,12 +3,12 @@ import type { RouteObject } from 'react-router-dom'
 import React from 'react';
 
 // 首页及登录页面需要直接加载
-import MainLayout from '../layout/MainLayout';
-import BaseLayout from '../layout/BaseLayout';
+import MainLayout from '@/layout/MainLayout';
+import BaseLayout from '@/layout/BaseLayout';
 
 // 采用懒加载提高用户体验
-const Home = React.lazy(() => import('../page/Home'));
-const ArticlesList = React.lazy(() => import( '../page/Articles/ArticlesList'));
+const Home = React.lazy(() => import('@/page/Home'));
+const ArticlesList = React.lazy(() => import( '@/page/Articles/ArticlesList/Container'));
 
 const routes: RouteObject[] = [
     {
